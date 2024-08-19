fd12404dd05dc719cd98e0e9602ebe6d5d1dc19b
AslamSuneer <103572084+AslamSuneer@users.noreply.github.com>
2024-08-19 21:11:58 +0530

    Update DissimilarityMatrixFile.py

--- DissimilarityMatrixFile.py
+++ DissimilarityMatrixFile.py
@@ -42,20 +42,22 @@ for row in dissimilarity_matrix:
     print(row)
 
 mixed_att = df
-
 numr = mixed_att.shape[0]
 dissimilarity_matrix = [[0.0 for _ in range(numr)] for _ in range(numr)]
-
 for i in range(numr):
     for j in range(numr):
         if i >= j:
-            if mixed_att.iloc[i].dtype == 'number' and mixed_att.iloc[j].dtype == 'number':
-                dissimilarity_matrix[i][j] = sum([abs(mixed_att.iloc[i, k] - mixed_att.iloc[j, k]) for k in range(len(mixed_att.columns))])
-            else:
-                dissimilarity_matrix[i][j] = sum([1 if mixed_att.iloc[i, k] != mixed_att.iloc[j, k] else 0 for k in range(len(mixed_att.columns))])               
+            dissimilarity = 0
+            for col in mixed_att.columns.tolist():
+                if mixed_att[col].dtype == 'object':
+                    dissimilarity += 1 if mixed_att[col][i] != mixed_att[col][j] else 0
+                else:
+                    dissimilarity += abs(mixed_att[col][i] - mixed_att[col][j])
+            dissimilarity_matrix[i][j] = round(dissimilarity/cols,2)
         else:
             dissimilarity_matrix[i][j] = 0
 
 print('Dissimilarity matrix for mixed attributes:')
 for row in dissimilarity_matrix:
     print(row)
+    
